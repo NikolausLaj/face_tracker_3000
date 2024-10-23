2603a8b1992c0811fca980b1f4140f37a4c6e327
Nikolaus Lajtai <nikolaus.lajtai@gmx.at>
2024-10-23 09:43:12 +0000

    add talker and listener

--- README.md
+++
@@ -1,1 +0,0 @@
-# face_tracker_3000
--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'face_tracker_3000'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=[package_name],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='Face Tracker 3000 ROS2 Python Package',
-    license='Apache License 2.0',
+    maintainer='ubuntu',
+    maintainer_email='nikolaus.lajtai@gmx.at',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'face_tracker_3000_node = face_tracker_3000.face_tracker_3000_node:main',
+            'talker = py_pubsub.publisher_member_function:main',
+            'listener = py_pubsub.subscriber_member_function:main',
         ],
     },
 )
